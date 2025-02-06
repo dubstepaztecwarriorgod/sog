0783002b7de29eb25b0e8c553fdd54675f1d30cf
Collin <dubstepaztec35@gmail.com>
2025-02-05 18:16:07 -0800

    added parser

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 mod token;
 mod parser;
+mod asm;
 
 fn main() {
     let expr = "2 - 3 * (14 / 2)";
     let tokens = token::tokenize(expr.as_bytes());
     println!("{tokens:?}");
+    let ast = parser::Parser::new(tokens).parse();
+    println!("{ast:?}")
 }
--- src/parser.rs
+++ src/parser.rs
@@ -1,26 +1,92 @@
 use crate::token::Token;
 
-pub enum Operation {
-    Add,
-    Sub,
-    Mul,
-    Div,
-    Exp
+#[derive(Debug)]
+#[allow(unused)]
+pub enum Expr {
+    Num(i32),
+    Op {
+        left: Box<Expr>,
+        op: Token,
+        right: Box<Expr>,
+    },
 }
 
-pub enum Node {
-    Number(i32), 
-    Operator {
-        operation: Operation,
-        left: Box<Node>,
-        right: Box<Node>
+pub struct Parser {
+    tokens: Vec<Token>,
+    cursor: usize,
+}
+
+impl Parser {
+    pub fn new(tokens: Vec<Token>) -> Self {
+        Parser { tokens, cursor: 0 }
+    }
+
+    pub fn parse(&mut self) -> Expr {
+        self.parse_expr()
+    }
+
+    fn parse_expr(&mut self) -> Expr {
+        let mut expr = self.parse_term();
+        while self.peek().map_or(false, |t| matches!(t, Token::Add | Token::Sub)) {
+            let op = self.next().unwrap();
+            let right = self.parse_term();
+            expr = Expr::Op {
+                left: Box::new(expr),
+                op,
+                right: Box::new(right),
+            };
+        }
+        expr
+    }
+
+    fn parse_term(&mut self) -> Expr {
+        let mut expr = self.parse_factor();
+        while self.peek().map_or(false, |t| matches!(t, Token::Mul | Token::Div)) {
+            let op = self.next().unwrap();
+            let right = self.parse_factor();
+            expr = Expr::Op {
+                left: Box::new(expr),
+                op,
+                right: Box::new(right),
+            };
+        }
+        expr
+    }
+
+    fn parse_factor(&mut self) -> Expr {
+        let mut expr = self.parse_primary();
+        while self.peek() == Some(&Token::Exp) {
+            self.next();
+            let right = self.parse_factor();
+            expr = Expr::Op {
+                left: Box::new(expr),
+                op: Token::Exp,
+                right: Box::new(right),
+            };
+        }
+        expr
+    }
+
+    fn parse_primary(&mut self) -> Expr {
+        match self.next() {
+            Some(Token::Num(n)) => Expr::Num(n),
+            Some(Token::LParen) => self.parse_expr(),
+            _ => panic!("Unrecognized token"),
+        }
+    }
+
+    fn peek(&self) -> Option<&Token> {
+        self.tokens.get(self.cursor)
+    }
+
+    fn next(&mut self) -> Option<Token> {
+        if self.cursor < self.tokens.len() {
+            let token = self.tokens[self.cursor].clone();
+            self.cursor += 1;
+            Some(token)
+        } else {
+            None
+        }
     }
 }
 
-fn precedence(op: Operation) -> i32 {
-    match op {
-        Operation::Add | Operation::Sub => 1,
-        Operation::Div | Operation::Mul => 2,
-        Operation::Exp => 3
-    }
-}
